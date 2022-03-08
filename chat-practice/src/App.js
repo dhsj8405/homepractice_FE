67fed54e0f4c51a4b520e03f03d9b118ac64e508
woo <dhsj8405@naver.com>
2022-03-09 03:03:57 +0900

    메세지 전송 성공 DB 연동안됨

--- chat-practice/src/App.js
+++ chat-practice/src/App.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react';
 // import * as Stomp from "webstomp-client";
 import { Stomp } from '@stomp/stompjs';
-import * as SockJS from "sockjs-client";
+// import { Stomp } from '@stomp/rx-stomp';
+import SockJS from "sockjs-client";
 
 const App = () => {
     const [ str, setStr ] = useState('');
@@ -11,8 +12,27 @@ const App = () => {
     // const [enterStatus, setEnterStatus] = useState(false);
     const [content, setContent]= useState('');
     const [inputMessage, setInputMessage]= useState('');
-    var sock = new SockJS('http://localhost:9099/chat')
-    var client = Stomp.over(sock);
+    
+    // var sock = new SockJS('http://localhost:9099/stomp')
+    // var client = Stomp.over(sock);
+    
+    var client = Stomp.over( () => {
+        return new SockJS('http://localhost:9099/stomp')
+    });
+
+    // client.reconnect_delay = 5000;
+
+    // const client = new Stomp.Client({
+    //     brokerURL: 'ws://localhost:9099/ws',
+    //     debug: function (str) {
+    //       console.log(str);
+    //     },
+    //     reconnectDelay: 5000,
+    //     heartbeatIncoming: 4000,
+    //     heartbeatOutgoing: 4000,
+    //   });
+
+    
     
     // 메시지 입력 핸들러
     const inputMessageHandler = (e) => {
@@ -23,16 +43,34 @@ const App = () => {
     function enterkey(e) {
         e.preventDefault();
         if (window.event.keyCode == 13) {
+            console.log("들어오냐?")
             // 메시지 보내기
             sandMessage(inputMessage);
         }
     }
      // 메시지 전송 함수
      var sandMessage = (msg) => {
-         console.log("메세지 전송함수까지 들어옴")
-         console.log(msg)
-        // client.send('/app/chat/enter',{},JSON.stringify({roomId: 1, roomName:"1번방",userNo : 1}));
-        client.send('/app/chat/message', {}, JSON.stringify({ chatRoomNo: 1, msg: msg, send_user_no: 1 }));
+        // client.send('/app/chat/message', {}, JSON.stringify({ chatRoomNo: 1, msg: msg, send_user_no: 1 }));
+        
+        if(client.disconnect){
+            // connect 돼있으면 알아서 Already ACTIVE, ignoring request to activate 라고 말해주면서 연결안하고 넘어감 
+            // 연결이 끊기기전(메시지 빠르게연속으로보낼때)에 보내면 메시지 전송안되기때문에 밑에 else로 연결돼있을때도 메시지 보낼 수 있어야함
+            client.connect({}, ()=>{
+                client.publish({
+                    destination: '/app/chat/message',
+                    body: JSON.stringify({ chatRoomNo: 1, msg: msg, send_user_no: 1 }),
+                    header: {}
+                });
+            })
+        }else{
+            client.publish({
+                destination: '/app/chat/message',
+                body: JSON.stringify({ chatRoomNo: 1, msg: msg, send_user_no: 1 }),
+                header: {}
+            });
+        }
+        
+        
     }
     
     // 소켓연결
@@ -46,14 +84,16 @@ const App = () => {
             //subscribe(path, callback)으로 메세지를 받을 수 있음
             client.subscribe('/topic/chat/room/a',(chat)=>{
                 var content = JSON.parse(chat.body);
-                
                 setContent(content.inputMessage)
             })
-            
+            console.log("fdfdsafdsafdsafdsafsafdasfdasfdsafasdfasdfdsa"+client);
+            console.log(client);
             //send(path, header, message)로 메세지를 보낼 수 있음 / *채팅방에 참여 
             client.send('/app/chat/enter',{},JSON.stringify({roomId: 1, roomName:"1번방",userNo : 1}));
+            client.activate();
+
         })
-        
+
 
     }
     // 첫 렌더링에만 호출하기(매개변수로 빈배열)
@@ -61,7 +101,7 @@ const App = () => {
         axios({
             url: 'http://localhost:9099/main',
             method: 'GET'
-        },{withCredentials: true}).then((res)=> {
+        }).then((res)=> {
             setStr(res.data);
         })
         socketConn();
