61c71069157bef1faf83cc863987d8c67e305f43
dhsj8405 <dhsj8405@naver.com>
2022-04-22 17:35:02 +0900

    redux practice 시작

--- redux-practice/src/index.js
+++ redux-practice/src/index.js
@@ -1,12 +1,22 @@
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';           // ex6의 언어적인 모듈시스템 : 언어적으로 표현
-import React from 'react';
+import App from './App.js';
+import { createStore } from "redux";
+import { Provider } from "react-redux"; 
+import rootReducer from "./module/index";
+
+const store = createStore(rootReducer);
+
 
 ReactDOM.render(
-    
-    <App />,
-    // App(), 
-    
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
     
     )   
+/*
+    - Provider
+    react-redux라이브러리 안에 있는 컴포넌트
+    리액트 앱에 스토어를 쉽게 연결하기 위한 컴포넌트
+ */
